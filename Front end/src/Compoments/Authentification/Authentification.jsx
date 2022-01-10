b0ff0f55aa9a92825941da647f59ebd00989abc6
SalaheddineOBT <91952603+SalaheddineOBT@users.noreply.github.com>
2022-01-10 14:09:53 +0100

    save changes

--- Front end/src/Compoments/Authentification/Authentification.jsx
+++ Front end/src/Compoments/Authentification/Authentification.jsx
@@ -1,30 +1,58 @@
 import React,{Component} from "react";
 
 export default class Athentification extends Component{
+    state={
+        activelogin:true,
+        activesignup:false
+    }
+
+    toogleSinUp=()=>{
+        const {activesignup}=this.state;
+        this.setState({activesignup:!activesignup});
+        const {activelogin}=this.state;
+        this.setState({activelogin:!activelogin});
+    }
+
+    toogleLogin=()=>{
+        const {activesignup}=this.state;
+        this.setState({activesignup:!activesignup});
+        const {activelogin}=this.state;
+        this.setState({activelogin:!activelogin});
+    }
+
+    SignUp=(e)=>{
+        e.preventDefault();
+    }
+
+    Login=(e)=>{
+        e.preventDefault();
+    }
+
     render(){
         return(
-            <div className="main">
-                <input type="checkbox" id="chk" aria-hidden="true" />
+            <div className="container">
 
-                <div className="SignUp">
-                    <form>
-                        <label htmlFor="chk" aria-hidden="true">Sign Up</label>
-                        <input type="text" name="username" placeholder="UserName" required />
-                        <input type="email" name="email" placeholder="Email" required />
-                        <input type="password" name="password" placeholder="Password" required />
-                        <input type="password" name="confirm" placeholder="Confirmer Password" required />
-                        <button>Sign Up</button>
-                    </form>
-                </div>
-
-                <div className="Login">
-                    <form>
-                        <label htmlFor="chk" aria-hidden="true">Login</label>
-                        <input type="email" name="lemail" placeholder="Email" required />
-                        <input type="password" name="lpassword" placeholder="Password" required />
-                        <button>Login</button>
-                    </form>
-                </div>
+                <form className={this.state.activesignup ? "SignUp active-SignUp" : "SignUp inactive-SignUp"} onSubmit={this.SignUp}>
+                    <h3>Sign Up</h3>
+                    <input type="text" name="username" placeholder="UserName" />
+                    <input type="email" name="email" placeholder="Email" />
+                    <input type="password" name="password" placeholder="Password" />
+                    <input type="password" name="confirm" placeholder="Confirmer Password" />
+                    <div className="btns">
+                        <button type="submit" className="btnLogin" onClick={this.toogleSinUp} >Login</button>
+                        <button type="submit" className="btnSub" >Sign Up</button>
+                    </div>
+                </form>
+                
+                <form className={this.state.activelogin ? "Login active-Login" : "Login inactive-Login"} onSubmit={this.Login}>
+                    <h3>Login</h3>
+                    <input type="email" name="lemail" placeholder="Email" />
+                    <input type="password" name="lpassword" placeholder="Password" />
+                    <div className="btns">
+                        <button type="submit" className="btnBack" onClick={this.toogleLogin} >Sign Up</button>
+                        <button type="submit" className="btnSub" >Login</button>
+                    </div>
+                </form>
 
             </div>
         )

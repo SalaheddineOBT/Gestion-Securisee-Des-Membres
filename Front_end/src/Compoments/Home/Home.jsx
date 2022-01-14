c2409c8908bbc65a286866af559b8695cec1ae1b
SalaheddineOBT <91952603+SalaheddineOBT@users.noreply.github.com>
2022-01-14 15:45:29 +0100

    save changes

--- Front_end/src/Compoments/Home/Home.jsx
+++ Front_end/src/Compoments/Home/Home.jsx
@@ -1,116 +1,212 @@
-import React, { Component, Fragment } from "react";
-import List from "./Liste";
-import axios from "axios";
-
-const Axios=axios.create({
-    baseURL:"http://localhost/Back%20end/Operations/"
-});
+import React,{Component,Fragment,createRef} from "react";
+import sha256 from "sha256";
+import Liste from "./Liste";
 
 export default class Home extends Component{
+
     constructor(props){
         super(props);
+        this.inputName=createRef();
+        this.inputEmail=createRef();
+        this.inputPassword=createRef();
+        this.inputConfirm=createRef();
     }
 
     state={
+        err:'',
+        active:false,
         users:[],
-        error:'',
-        succe:''
-    }
+        user:{
+            username:'',
+            email:'',
+            password:'',
+            confirm:''
+        },
+        user1:{},
+        f:false
+    };
 
     componentDidMount(){
         this.listing();
-        document.title="Page d'accueil";
-    }
+        document.title="Home Page";
+    };
 
-    err=(val,type)=>{
-        if(type){
-            this.setState({succe:val});
-        }else{
-            this.setState({error:val});
-        }
-    }
-
-    listing=async()=>{
-        await Axios.get("Listing.php").then(res=>{
-            if(res.data.success){
-                this.setState({users:res.data.data});
-            }
+    listing=async ()=>{
+        await fetch('http://localhost/Crud%20API%20PHP/Operations/Listing.php')
+        .then(res=>{
+            return res.json();
+        }).then(data=>{
+            this.setState({users:data.data});
         }).catch(e=>{
             console.log(e);
         });
+    };
+
+    OnChanging=()=>{
+        const i={
+            username:this.inputName.current.value,
+            email:this.inputEmail.current.value,
+            password:this.inputPassword.current.value,
+            confirm:this.inputConfirm.current.value
+        }
+        this.setState({user:i});
     }
 
-    onSub=(e)=>{
-        e.preventDefault();   
+    Onsub=(e)=>{
+        e.preventDefault();
+    };
+
+    OnSubmited=async(e)=>{
+        e.preventDefault();
+        const {user}=this.state;
+        if(user.password.length < 8){
+            this.setState({err:"Password Must Be at least 8 characters !"});
+        }else if(user.password!==user.confirm){
+            this.setState({err:"Confirm Password Is Incorrect !"});
+        }else{
+            await fetch("http://localhost/Crud%20API%20PHP/Operations/Create.php",{
+                method:"POST",
+                headers:{'Content-Type': 'application/json'},
+                body:JSON.stringify({
+                    username:user.username,
+                    email:user.email,
+                    password:sha256.x2(user.confirm)
+                })
+            }).then(res=>{
+                return res.json();
+            }).then(data=>{
+                if(data.success){
+                    alert(data.Message);
+                    this.listing();
+                    this.setState({err:""});
+                    this.setState({user:{
+                        username:'',
+                        email:'',
+                        password:'',
+                        confirm:''
+                    }});
+                    this.inputName.current.value="";
+                    this.inputEmail.current.value="";
+                    this.inputPassword.current.value="";
+                    this.inputConfirm.current.value="";
+                    this.toogle();
+
+                }else{
+                    this.setState({err:data.Message});
+            }
+
+            }).catch(e=>{
+                console.log(e);
+            });
+        }
     }
 
+    toogle=()=>{
+        const {active}=this.state;
+        this.setState({active:!active});
+        this.inputName.current.value="";
+        this.inputEmail.current.value="";
+        this.inputPassword.current.value="";
+        this.inputConfirm.current.value="";
+    }
 
+    Read=async (id,v)=>{
+        if(id && Number.isInteger(id)){
+            await fetch(`http://localhost/Crud%20API%20PHP/Operations/Read.php?id=${id}`,{
 
-    render() {
+            }).then(res=>{
+                return res.json();
+            }).then(data=>{
+                if(data.success){
+                    this.setState({user1:data.data});
+                    this.setState({f:v});
+                }else{
+                    alert(data.Message);
+                }
+            }).catch(e=>{
+                console.log(e);
+            });
+        }
+    }
+
+    toogleF=()=>{
+        this.setState({f:false});
+    }
+    
+    render(){
         const {id}=this.props;
+
         const lister=this.state.users.map((u,i)=>{
             return(
                 <Fragment key={i}>
-                    <List users={u} id={id} logined={this.props.logined} err={this.err} index={i} listing={this.listing} />
+                    <Liste users={u} id={id} Read={this.Read} logined={this.props.logined} index={i} listing={this.listing} />
                 </Fragment>
             )
         });
 
-        return (
-            <div className="HomePage">
-                <nav>
-                    <span className="tit">
-                        <h1>
-                            user Connecté :
-                            <span className="h">
-                                {this.props.v}
-                            </span>
-                        </h1>
-                        <button type="submit" title="Déconnecté" className="logout" onClick={()=>this.props.logout()}>LogOut</button>
-                    </span>
-                </nav>
-                <div className="cont">
-                    <div className="toping">
-                        <h1>
-                            Members Informations :
-                        </h1>
-                        <button type="submit" title="Ajouter User" className="btni">Create New</button>
-                    </div>
-                    <form className="befortbl" onSubmit={this.onSub}>
-                        <table border="1">
-                            <thead className="stiky">
-                                <tr>
-                                    <th>Photo</th>
-                                    <th>User Name</th>
-                                    <th>Email</th>
-                                    <th>Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {lister}
-                            </tbody>
-                        </table>
+        return(
+            <>
+                <div className={(this.state.active) ? "active" : "Create"}>
+                    <form onSubmit={this.OnSubmited} className="form">
+                        <h1>Create New User</h1>
+                        {/* {this.state.err ? (<span className="error">{this.state.err}</span>) : null} */}
+                        <label htmlFor="username">User Name :</label>
+                        <input type="text" name="username" placeholder="Enter Your User Name :" ref={this.inputName} defaultValue={this.state.user.username} required onChange={this.OnChanging} />
+                        <label htmlFor="email">Email :</label>
+                        <input type="email" name="email" placeholder="Enter Your Email :" ref={this.inputEmail} defaultValue={this.state.user.email} required onChange={this.OnChanging} />
+                        <label htmlFor="password">Passowrd :</label>
+                        <input type="password" name="password" placeholder="Enter Your Password :" ref={this.inputPassword} defaultValue={this.state.user.password} required onChange={this.OnChanging} />
+                        <label htmlFor="confirm">Confirme Passowrd :</label>
+                        <input type="password" name="confirm" placeholder="Confirm Your Password :" ref={this.inputConfirm} defaultValue={this.state.user.confirm} required onChange={this.OnChanging} />
+                        <button type="submit">ADD</button>
+                        <svg xmlns="http://www.w3.org/2000/svg" onClick={this.toogle} class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
                     </form>
                 </div>
-                <div className="ero">
+                
+                <div className="HomePage">
+                    <nav>
+                        <span className="tit">Dashboard</span>
+                        <h1>User Connecté : <span className="h">{this.props.v}</span></h1>
+                        <button className="logout" type='submit' onClick={()=>this.props.logout()}>Logout</button>
+                    </nav>
                     <div className="cont">
-                        {this.state.error}
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
+                        <div className="toping">
+                            <h1>Members Information</h1>
+                            <button className="btni" type="submit" onClick={this.toogle}>Create New</button>
+                        </div>
+                        <form className="befortbl" onSubmit={this.Onsub}>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Photo</th>
+                                        <th>User Name</th>
+                                        <th>Email</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {lister}
+                                </tbody>
+                            </table>
+                        </form>
                     </div>
                 </div>
-                
-                <div className="suc">
-                    <div className="cont">
-                        {this.state.succe}
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                        </svg>
+
+                <div className={this.state.f ? "information" : "informaleft"}>
+                    <div className="containn">
+                        <h1>User Information</h1>
+                        <ul>
+                            <li><span className="in">ID : </span>{this.state.user1.ID} </li>
+                            <li><span className="in">User Name : </span> {this.state.user1.UserName}</li>
+                            <li><span className="in">Email : </span>{this.state.user1.Email} </li>
+                            <li><span className="in">Create in : </span> {this.state.user1.CreateDate}</li>
+                        </ul>
+                        <button onClick={this.toogleF}>Close</button>
                     </div>
                 </div>
-                
-            </div>
+            </>
         )
     }
 }
--- Front_end/src/Compoments/Home/Liste.jsx
+++ Front_end/src/Compoments/Home/Liste.jsx
@@ -1,90 +1,135 @@
-import React, { Component, createRef } from "react";
-import axios from "axios";
+import React,{Component, createRef} from "react";
+import img from "./../../images/bg.png";
 
-export default class List extends Component{
-
+export default class Liste extends Component{
     constructor(props){
         super(props);
-        this.inputemail=createRef();
-        this.inputUseName=createRef();
-    }
-
+        this.inputUsername=createRef();
+        this.inputEmail=createRef();
+    };
     state={
-        updated:false
-    }
-
-    toogleUpdate=()=>{
-        const {updated}=this.state;
-        this.setState({updated:!updated});
-    }
-
-    Update=async(idd)=>{
-        let name=this.inputUseName.current.value;
-        let eml=this.inputemail.current.value
-        if(idd && Number.isInteger(idd) && name && eml){
-          await fetch("http://localhost/Crud%20API%20PHP/Operations/Update.php",{
-              method:"PUT",
-              headers:{'Content-Type': 'application/json'},
-              body:JSON.stringify({id:idd,username:name,email:eml})
-          }).then(res=>{
-              return res.json();
-          }).then(data=>{
-            if(data.success){
-                alert(data.Message);
-                this.props.listing();
-                this.toogleUpdate();
-                this.props.err(data.Message,true);
-            }else{
-                this.props.err(data.Message,false);
-            }
-          }).catch(e=>{
-              console.log(e);
-          })
-        }else{
-            this.props.err("All fields are required !",false);
+        isUpdate:false,
+        err:''
+    };
+    update=async (idd)=>{
+        let name=this.inputUsername.current.value;
+        let eml=this.inputEmail.current.value;
+        const emailRegex=/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if(name && eml && idd){
+            await fetch("http://localhost/Crud%20API%20PHP/Operations/Update.php",{
+                method:"PUT",
+                headers:{'Content-Type': 'application/json'},
+                body:JSON.stringify({id:idd,username:name,email:eml})
+            }).then(res=>{
+                return res.json();
+            }).then(data=>{
+                if(!emailRegex.test(eml)){
+                    this.setState({err:"Email Forma Incorrect !"});
+                }else{
+                    this.setState({err:""});
+                    if(data.success){
+                        if(this.props.id===idd){
+                            this.toogleUpdate();
+                            this.props.listing();
+                            alert(data.Message);
+                            this.props.logined(true,idd,name);
+                        }else{
+                            this.toogleUpdate();
+                            this.props.listing();
+                            alert(data.Message);
+                        }
+                    }else{
+                        alert(data.Message);
+                    }
+                }
+            }).catch(e=>{
+                console.log(e);
+            });
         }
-    }
-
+    };
+    delete=async (idd) =>{
+        if(window.confirm("Are You Really Wante Delete This User ?")){
+            await fetch("http://localhost/Crud%20API%20PHP/Operations/Delete.php",{
+                method:"DELETE",
+                headers:{'Content-Type': 'application/json'},
+                body:JSON.stringify({id:idd})
+            }).then(res=>{
+                return res.json();
+            }).then(data=>{
+                if(data.success){
+                    if(this.props.id==idd){
+                        this.props.logined(false,'','');
+                        window.location.reload();
+                    }else{
+                        alert(data.Message);
+                        this.props.listing();
+                    }
+                }else{
+                    alert(data.Message);
+                }
+            }).catch(e=>{
+                console.log(e);
+            });
+        }
+    };
+    
+    toogleUpdate=()=>{
+        const {isUpdate}=this.state;
+        this.setState({isUpdate:!isUpdate});
+    };
+    
     render(){
         const {users,index}=this.props;
         const lister=()=>{
             return(
                 <tr>
-                    <td><img src="" alt="image" /></td>
+                    <td>
+                        <img src={img} alt="Image" />
+                    </td>
                     <td>{users.UserName}</td>
                     <td>{users.Email}</td>
                     <td>
-                        <button type="submit" className="btns" title="Read User">Read</button>
-                        <button type="submit" className="btns" title="Update User" onClick={this.toogleUpdate}>Update</button>
-                        <button type="submit" className="btns" title="Delete User">Deelete</button>
+                        <svg xmlns="http://www.w3.org/2000/svg" onClick={this.toogleUpdate} title="Update User" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" type="submit" onClick={()=>this.delete(parseInt(users.ID))} title="Deelete User" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" onClick={()=>this.props.Read(parseInt(users.ID),true)} title="Read User" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
                     </td>
                 </tr>
             )
-        }
-
+        };
         const frm=()=>{
             return(
-                <tr>
+                <tr key={index}>
                     <td>
-                        <img src="" alt="image" />
+                        <input type="file" name="image" accept="image/*" />
+                    </td>
+                    <td><input type="text" ref={this.inputUsername} defaultValue={users.UserName} required /></td>
+                    <td>
+                        <input type="email" ref={this.inputEmail} defaultValue={users.Email} required />
+                        { (this.state.err) ? (<span className="error">{this.state.err}</span>) : null}
                     </td>
                     <td>
-                        <input type="text" ref={this.inputUseName} name="username" defaultValue={users.UserName} required />
-                    </td>
-                    <td>
-                        <input type="text" ref={this.inputemail} name="email" defaultValue={users.Email} required />
-                    </td>
-                    <td>
-                        <button type="submit" className="btn" onClick={()=>this.Update(users.ID)}>Save</button>
-                        <button type="submit" className="btn" onClick={this.toogleUpdate}>Cancel</button>
+                        <svg xmlns="http://www.w3.org/2000/svg" onClick={()=>this.update(users.ID)} title="Save" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"  title="Cancel button" viewBox="0 0 20 20" onClick={this.toogleUpdate} fill="currentColor">
+                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                        </svg>
                     </td>
                 </tr>
             )
-        }
-
+        };
         return(
             <>
-                {!this.state.updated ? lister() : frm()}
+                {!this.state.isUpdate ? lister() : frm() }  
             </>
         )
     }
